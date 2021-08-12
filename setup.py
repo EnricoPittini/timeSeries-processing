a2c5e2fc1e6002b45ba80b28dd1c5f0a1345cedd
Enrico Pittini <pittinienrico@hotmail.it>
2021-08-12 11:02:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name="timeSeries-processing",
-  version="0.0.1",
+  version="0.0.2",
   py_modules =["timeSeries_processing"],
   description="Library which processes time series datasets",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   url="https://github.com/EnricoPittini/timeSeries-processing",
   author="Enrico Pittini",
   author_email="pittinienrico@hotmail.it",
@@ -12,4 +17,9 @@ setup(
   install_requires=['matplotlib',
                     'pandas',
                     'sklearn' ],
+  classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
 )
